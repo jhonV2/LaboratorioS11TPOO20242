d7eb5390f338b3713a8dc4de71498f8d108fd90f
Jhon Apaza <jhonangelapazapariapaza@gmail.com>
2024-10-25 18:29:15 -0500

    Add files via upload
    no se si aparecerán los botones añadidos

---
+++ src/semana11/Semana11.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana11;
+import Pantalla.Calculadora;
+/**
+ *
+ * @author user
+ */
+public class Semana11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora ventana = new Calculadora();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+    
+}
